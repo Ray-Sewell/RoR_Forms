6e265c5fbf95bc587924815746f30563725cc348
Ray <sewellray11+git@googlemail.com>
2021-04-22 15:43:38 +0100

    added routes required to edit users, display request errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,14 +6,30 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            redirect_to new_user_path
+            redirect_to root_path
         else
             render :new
+        end
+    end
+
+    def edit
+        @user = find_user
+    end
+
+    def update
+        @user = find_user
+        if @user.update(user_params)
+            redirect_to root_path
+        else
+            render :edit
         end
     end
 
     private
         def user_params
             params.require(:user).permit(:username,:password,:email)
+        end
+        def find_user
+            User.find(params[:id])
         end
 end
